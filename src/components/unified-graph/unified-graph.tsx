1029cc78fbd7fa7b906d3469facb898e01759f54
Harry Watson <18227750+watanaberyunosuke@users.noreply.github.com>
2026-03-26 21:34:53 +1100

    update graph feature (#7)

--- src/components/unified-graph/unified-graph.tsx
+++ src/components/unified-graph/unified-graph.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import type { GraphData } from "@/data/graph-data";
 import { GraphSidebar } from "./graph-sidebar";
 import { ForceGraphView } from "./force-graph-view";
 
-export default function UnifiedGraph() {
+interface UnifiedGraphProps {
+  readonly data: GraphData;
+}
+
+export default function UnifiedGraph({ data }: UnifiedGraphProps) {
   return (
     <div className="fixed inset-0 z-50 bg-background flex overflow-hidden">
-      <GraphSidebar />
+      <GraphSidebar data={data} />
       <div className="flex-1 relative">
-        <ForceGraphView />
+        <ForceGraphView data={data} />
       </div>
     </div>
   );
