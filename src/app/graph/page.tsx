47433442e02fcaae7fe344ab1f1156eecfbedadb
Harry Watson <18227750+watanaberyunosuke@users.noreply.github.com>
2026-03-26 21:36:40 +1100

    Fix/icon (#8)
    * Added uni icon
    
    * Update vis and work icon
    
    * add work ending year
    
    * fix typo

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -4,7 +4,7 @@ import { generateGraphData } from "@/data/graph-data";
 
 export const metadata = {
   title: "Harry's Unified Graph",
-  description: "An interactive visualisation of blog posts and their MDX tags.",
+  description: "An interactive visualisation of the portfolio's structure and content.",
 };
 
 export default function GraphPage() {
